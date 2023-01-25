71087356986c4d978850a2721f925d578b60e125
blackkamenrider <hosias.souza@gmail.com>
2023-01-25 12:18:25 -0300

    Project created

---
+++ backend/src/test/java/com/hosias/testbackend/TestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosias.testbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
